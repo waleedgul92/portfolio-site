a64f9c0d75ba508101ead715f5087437133199cf
Waleed Gul <84980384+waleedgul92@users.noreply.github.com>
2023-10-02 21:26:41 +0500

    Update portfolio.js

--- src/portfolio.js
+++ src/portfolio.js
@@ -795,7 +795,7 @@ const contactPageData = {
     title: "Blogs",
     subtitle:
       "For individual fundamental empowerment, I like to write powerful lessons that create impact on each of the reader individually to change the core of their character.",
-    link: "https://blogs.ashutoshhathidara.com/",
+    link: "https://medium.com/@hwaleed0035/",
     avatar_image_path: "blogs_image.svg",
   },
   addressSection: {
