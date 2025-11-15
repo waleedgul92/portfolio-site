d7941a35fcbefdfc9219a84c49ec405fc4b027c3
Waleed Gul <84980384+waleedgul92@users.noreply.github.com>
2025-11-15 12:18:38 +0000

    Update ProjectCards.js

--- src/pages/projects/ProjectCards.js
+++ src/pages/projects/ProjectCards.js
@@ -4,24 +4,53 @@ import Button from "react-bootstrap/Button";
 import { BiLinkExternal } from "react-icons/bi";
 
 function ProjectCards(props) {
+  // Handle image click to open GitHub link
+  const handleImageClick = () => {
+    if (props.ghLink) {
+      window.open(props.ghLink, "_blank", "noopener,noreferrer");
+    }
+  };
+
   return (
     <Card className="project-card-view">
-      <Card.Img variant="top" src={props.imgPath} alt="card-img" />
+      {/* Clickable Image */}
+      <Card.Img 
+        variant="top" 
+        src={props.imgPath} 
+        alt="card-img"
+        onClick={handleImageClick}
+        style={{ cursor: props.ghLink ? 'pointer' : 'default' }}
+        title={props.ghLink ? "Click to view on GitHub" : ""}
+      />
       <Card.Body>
         <Card.Title>{props.title}</Card.Title>
         <Card.Text style={{ textAlign: "justify" }}>
           {props.description}
         </Card.Text>
-        <Button variant="primary" href={props.link} target="_blank">
-          <BiLinkExternal /> &nbsp;
-          {props.isBlog ? "View Blog" : "View Project"}
-        </Button>
-        <Button variant="primary" href={props.link} target="_blank">
-          <BiLinkExternal /> &nbsp;
-          {props.isBlog ? "View Blog" : "View Project"}
-        </Button>
+        
+        {/* GitHub Link Button */}
+        {props.ghLink && (
+          <Button variant="primary" href={props.ghLink} target="_blank">
+            <BiLinkExternal /> &nbsp;
+            View Code
+          </Button>
+        )}
+        
+        {/* Demo/Project Link Button (if exists) */}
+        {props.demoLink && (
+          <Button 
+            variant="primary" 
+            href={props.demoLink} 
+            target="_blank"
+            style={{ marginLeft: "10px" }}
+          >
+            <BiLinkExternal /> &nbsp;
+            {props.isBlog ? "View Blog" : "Live Demo"}
+          </Button>
+        )}
       </Card.Body>
     </Card>
   );
 }
+
 export default ProjectCards;
